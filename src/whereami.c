11a71b346d602eae90088c1180cb8f0ce6c7f6d0
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-01-20 09:27:12 -0800

    MinGW _ReturnAddress Fix
    The assumption was that if the library was being built on Windows it was being built with MSVC++.  Therefore in the wai_getModulePath function the _ReturnAddress intrinsic was used unconditionally, which doesn't exist in GCC.

--- src/whereami.c
+++ src/whereami.c
@@ -126,7 +126,13 @@ int WAI_PREFIX(getModulePath)(char* out, int capacity, int* dirname_length)
 #pragma warning(push)
 #pragma warning(disable: 4054)
 #endif
-  if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCTSTR)_ReturnAddress(), &module))
+  if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCTSTR)
+  #if defined(_MSC_VER)
+  _ReturnAddress()
+  #else
+  __builtin_extract_return_addr(__builtin_return_address(0))
+  #endif
+  , &module))
 #if defined(_MSC_VER)
 #pragma warning(pop)
 #endif
